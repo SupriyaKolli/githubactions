fd6715d3620b2c43ddcdd3f68c426ef85791e898
SupriyaKolli <163094526+SupriyaKolli@users.noreply.github.com>
2024-03-22 10:46:19 +0530

    Update customersPage.ts

--- features/page_objects/customersPage.ts
+++ features/page_objects/customersPage.ts
@@ -1,98 +1,158 @@
-import { WebDriver, By } from 'selenium-webdriver';
+import { WebDriver, By,until } from 'selenium-webdriver';
 import { AbstractPageObject } from './AbstractPageObject';
 import { driver } from '../support/hook';
 
-export class CustomersPage extends AbstractPageObject {
-    private clickToClearFiltersButton = By.id('clear-button');
+export class loginPage extends AbstractPageObject
+{
+private userName = By.id("user-name");
+private passWord=By.id("password");
+private loginButton = By.id("login-button");
+private addToCart = By.name("add-to-cart-sauce-labs-backpack");
+private addshirttoCart = By.name("add-to-cart-sauce-labs-bolt-t-shirt");
+private cartLogo = By.xpath("//*[@id='shopping_cart_container']/a");
+private checkoutButton = By.id("checkout");
+private fName = By.id("first-name");
+private lName = By.id("last-name");
+private pCode = By.id("postal-code");
+private continueButton = By.id("continue");
+private finishButton = By.id("finish");
+private backHomeButton = By.id("back-to-products");
+private menuOption = By.id("react-burger-menu-btn");
+private logOutButton = By.id("logout_sidebar_link");
+private removebtn = By.xpath("//button[@class='btn btn_secondary btn_small cart_button' and @id='remove-sauce-labs-backpack']");
+private continueShopping = By.id("continue-shopping");
+private total = By.xpath("//div[@class='summary_subtotal_label']");
+private itemcost1 = By.xpath("//*[@id='inventory_container']/div/div[1]/div[2]/div[2]/div");
+private itemcost2 = By.xpath("//*[@id='inventory_container']/div/div[3]/div[2]/div[2]/div");
+private itemname1 = By.id("item_4_title_link");
+private itemname2 = By.id("item_1_title_link");
 
-    private searchInput = By.id('search-input');
+constructor() {
+         super(driver);
+     }
 
-    private searchColumn = By.id('search-column');
+open = (url:string) =>
+{
+    this.driver.get(url);
+}
+username = async(name:string)=>
+{
+    await this.driver.findElement(this.userName).sendKeys(name);
+    return this;
+}
+password = async(pwd:string) =>
+{
+    await this.driver.findElement(this.passWord).sendKeys(pwd);
+    return this;
+}
+loginbutton = async() =>
+{
+    await this.driver.findElement(this.loginButton).click();
+    return this;
+}
+addCart = async() =>
+{
+     await this.driver.findElement(this.addToCart).click();
+     return this;
+}
+shirtCart = async()=>
+{
+    await this.driver.findElement(this.addshirttoCart).click();
+    return this;
+}
+cart = async()=>
+{
+     await this.driver.findElement(this.cartLogo).click();
+     return this;
+}
+checkout = async() =>
+{
+     await this.driver.findElement(this.checkoutButton).click();
+     return this;
+}
+firstname = async(fname1:string) =>
+{
+     await this.driver.findElement(this.fName).sendKeys(fname1);
+     return this;
+}
+lastname = async(lname1:string) =>
+{
+     await this.driver.findElement(this.lName).sendKeys(lname1);
+     return this;
+}
+pincode = async(pin:string) =>
+{
+     await this.driver.findElement(this.pCode).sendKeys(pin);
+     return this;
+}
+continue = async() =>
+{
+     await this.driver.findElement(this.continueButton).click();
+     return this;
+}
+finish = async()=>
+{
+      await this.driver.findElement(this.finishButton).click();
+     return this;
+}
+backHome = async() =>
+{
+      await this.driver.findElement(this.backHomeButton).click();
+     return this;
+}
+menu = async() =>
+{
+      await this.driver.findElement(this.menuOption).click();
+     return this;
+}
+logOut = async() =>
+{
+      const element = await this.driver.findElement(this.logOutButton);
+     await this.driver.wait(until.elementIsVisible(element), 1000);
+     return element.click();
+}
+removeButton = async () =>
+ {
+    const element1 = await this.driver.findElement(this.removebtn);
+    await this.driver.wait(until.elementIsVisible(element1), 3000);
+    return element1.click();
+ }
 
-    private matchCase = By.id('match-case');
-
-    private summary = By.id('table-resume');
-
-    private searchTerm = By.id('search-slogan');
-
-    private searchResultsTable = By.xpath("//table");
-
-    constructor() {
-        super(driver);
-    }
-
-    /**
-     * Click on Clear Filters Button.
-     *
-     * @return the CustomersPage class instance.
-     */
-    clickClearFiltersButton = async () => {
-        await this.driver.findElement(this.clickToClearFiltersButton).click();
-        return this;
-    };
-
-    /**
-     * Set value to searchInput field.
-     *
-     * @param input String which should be typed into the field
-     * @return the CustomersPage class instance.
-     */
-    setSearchInput = async (input: string) => {
-        await this.driver.findElement(this.searchInput).sendKeys(input);
-        return this;
-    };
-
-    /**
-     * Set value to Search Column Drop Down List field.
-     *
-     * @param value String which should match with one of values visible on the
-     * dropdown
-     * @return the CustomersPage class instance.
-     */
-    selectSearchColumnByText = async (value: string) => {
-        await this.driver.findElement(
-            By.xpath(`//select[@id='search-column']/option[text()='${value}']`))
-            .click();
-        return this;
-    }
-
-    /**
-     * Set Match Case Checkbox field to required value.
-     *
-     * @param value boolean value of the checkbox status true - checked,
-     * false - unchecked
-     * @return the CustomersPage class instance.
-     */
-    setMatchCaseCheckboxField = async (value: boolean) => {
-        const checkboxState = await this.driver.findElement(this.matchCase).isSelected();
-        if (checkboxState !== value) {
-            await this.driver.findElement(this.matchCase).click();
-        }
-        return this;
-    };
-
-    getSummaryText = async () => {
-        return await this.driver.findElement(this.summary)
-            .getText();
-    };
-
-    getSearchTermText = async () => {
-        return await this.driver.findElement(this.searchTerm)
-            .getText();
-    };
-
-    getSearchInputText = async () => {
-        return await this.driver.findElement(this.searchInput)
-            .getText();
-    };
-
-    getSearchResultsTableText = async () => {
-        return await this.driver.findElement(this.searchResultsTable)
-            .getText();
-    };
-
-    open = async () => {
-        const pageUrl: string = "https://danieldelimata.github.io/sample-page/";
-        await this.driver.get(pageUrl);
-    };
+ continushop = async () =>
+ {
+    await this.driver.findElement(this.continueShopping).click();
+    return this;
+ }
+ Totalamount = async () =>
+ {
+        const element = await this.driver.findElement(this.total);
+        const text = await element.getText();
+        console.log(text);
+ }
+ item1 = async () =>
+ {
+    const element = await this.driver.findElement(this.itemcost1);
+    const text = await element.getText();
+    const element1 = await this.driver.findElement(this.itemname1);
+    const text1 = await element1.getText();
+     console.log(text1, ":", text);
+ }
+  item2 = async () =>
+  {
+     const element = await this.driver.findElement(this.itemcost2);
+     const text = await element.getText();
+     const element1 = await this.driver.findElement(this.itemname2);
+          const text1 = await element1.getText();
+           console.log(text1, ":", text);
+  }
+ title = async () =>
+ {
+    const title = await driver.getTitle();
+    console.log(title);
+ }
+ close = async () =>
+ {
+    await this.driver.close();
+    return this;
+ }
 }
